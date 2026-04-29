1dce5782421a1a8e2e048405a1ceec53e09967e0
Luoyu126 <chenyuny24@mails.tsinghua.edu.cn>
2026-04-29 08:07:51 +0000

    Deploying to gh-pages from @ Luoyu126/Luoyu126.github.io@cf801ebff1256f23ff694bb125a48920978711ea 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Luoyu126/chenyy.github.io",
+    "data-repo": "Luoyu126/Luoyu126.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
